e66ce144022e8c15ce4c565084b19273fd830b78
justillusionmia <whiskeypierre002@gmail.com>
2025-01-18 21:19:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,8 +37,9 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger-button');
     const mobileMenu = document.querySelector('.mobile-menu');
-  
-    hamburgerButton.addEventListener('click', () =>
-      mobileMenu.classList.toggle('active')
-    );
-  });
+
+    hamburgerButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+        hamburgerButton.classList.toggle('fixed');
+    });
+});
